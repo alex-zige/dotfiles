a08047462f1a791362aec3e0cc9b68ba26a00e56
Alex Li <alex.zige@gmail.com>
2014-03-08 18:44:53 +1300

    Add more Path env settings

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,11 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+#force iTerm to encode as utf-8
+export LANG=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+
+#Fix for postgress on Mac OS X
+export PATH=/usr/local/bin:$PATH
+
+export PATH=$PATH:/Applications/AWS-ElasticBeanstalk-CLI-2.3/eb/linux/python2.7/
+export PATH=$PATH:/usr/local/share/npm/bin/
