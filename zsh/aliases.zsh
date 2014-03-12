3c4798218df4bcc2c7b7319e258d97543a5928b1
Alex Li <alex.zige@gmail.com>
2014-03-12 22:39:10 +1300

    Add more aliases for cd an ls

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,7 @@
 alias reload!='. ~/.zshrc'
+alias ..='cd ..'            # Go up one directory
+alias ...='cd ../..'        # Go up two directories
+alias ....='cd ../../..'    # And for good measure
+alias l='ls -lah'           # Long view, show hidden
+alias la='ls -AF'           # Compact view, show hidden
+alias ll='ls -lFh'
